8c87183779ea268e42e2e7f3da7544e1373a4beb
michi-p31 <michellpovedab@gmail.com>
2024-08-08 17:52:05 -0500

    clasroom_teacher css y agrego ruta

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -4,6 +4,7 @@ import Categorias from "./screens/Categorias.jsx";
 import Cultivos from "./components/cultivos.jsx";
 import Informacion_plant from "./components/Informacion.jsx";
 import Student from "./screens/Classroom._student.jsx";
+import Teacher from "./screens/Classroom_teacher.jsx";
 import {Login} from './screens/Login.jsx'
 import {Monitoring} from './screens/Monitoring.jsx'
 import {About_us} from './screens/About_us.jsx'
@@ -18,6 +19,7 @@ const App = () => {
         <Route path="/categorias/:categoria/:nombreFruta" element={<Informacion_plant />} />
         <Route path='/Login' element={<Login />}/>
         <Route path='/ClassRoom_Student' element={<Student/>}/>
+        <Route path='/ClassRoom_Teacher' element={<Teacher/>}/>
         <Route path='/ClassRoom/Monitoring' element={<Monitoring/>}/>
         <Route path='/About_us' element={<About_us/>}/>
       </Routes>
---
+++ FRONTEND/src/screens/Classroom_teacher.jsx
@@ -0,0 +1,38 @@
+
+import { Link } from 'react-router-dom';
+import plant from "../assets/images/planta.png";
+import form from "../assets/images/formulario.png";
+import comment from "../assets/images/comentario.png";
+import styles from "../styles/Classroom_teacher.module.css";
+
+const Teacher = () => {
+  return (
+    <div>
+      <section className={styles.topCategorias}>
+        <h1 className={styles.title}>Aula Virtual</h1>
+        <div className={styles.containerCategoria}>
+          <div className={styles.cardCategorie}>
+            <Link to="/ClassRoom/Monitoring">
+              <img src={plant} alt="Estado De La Huerta" />
+              <h1>Estado De La Huerta</h1>
+            </Link>
+          </div>
+          <div className={styles.cardCategorie}>
+            <Link to="">
+              <img src={form} alt="Formulario monitoreo" />
+              <h1>Entregas</h1>
+            </Link>
+          </div>
+          <div className={styles.cardCategorie}>
+            <Link to="">
+              <img src={comment} alt="Subir Entregas" />
+              <h1>Estudiates</h1>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Teacher;
