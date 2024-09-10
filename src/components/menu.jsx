0a2f54d8cf8087db59277c10e6e45d74aa13e280
ZamoraNicolas <117843369+ZamoraNicolas@users.noreply.github.com>
2024-09-10 09:55:31 -0500

    Update menu.jsx

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -34,7 +34,7 @@ const Menu = () => {
             <nav id="nav">
               <ul>
                 <li><img src={cultivo}></img><Link to="/categorias">Cultivos</Link></li>
-                <li><img src={aula}></img><Link to="#">Aula virtual</Link></li>
+                <li><img src={aula}></img><Link to="/Login">Aula virtual</Link></li>
                 <li><img src={abonos}></img><Link to="/Abonos">Abonos Orgánicos</Link></li>
                 <li><img src={medicinal}></img><Link to="/Medicinal">Usos Medicinales</Link></li>
                 <li><img src={about_us} alt="About us" /><Link to='/About_Us'>Acerca de nosotros</Link></li>
