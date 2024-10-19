12fce49ccd0bc0bdead41cacd608b3e3eb30e510
Nicolas Zamora <nicolaszamoracalderon62@gmail.com>
2024-10-18 23:54:18 -0500

    Ya se pueden ver las clases y las instituciones

--- src/screens/Administrator_class.jsx
+++ src/screens/Administrator_class.jsx
@@ -1,18 +1,40 @@
-import NavBar from '../components/NavBar_Administrator'
-import styles from '../styles/Administrator_class.module.css';
+import NavBar from "../components/NavBar_Administrator";
+import styles from "../styles/Administrator_class.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Administrator_class = () => {
+  const [classes, setClasses] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/v1/getClasses"
+        );
+        setClasses(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <h1 className={styles.Titulo}>CLASES</h1>
-      <div className={styles.Class_space}>
-        <div className={styles.Class_info}>
-          <p>Codigo de la Clase: ###</p>
-          <p>Nombre de la Clase: ###</p>
+      {classes.map((Class) => (
+        <div key={Class.Id_Class} className={styles.Class_space}>
+          <div className={styles.Class_info}>
+            <p>Codigo de la Clase: {Class.Id_Class}</p>
+            <p>Nombre de la Clase: {Class.Class_Name}</p>
+          </div>
+          <div className={styles.Section_Buttons}>
+            <button className={styles.Button_edit}>Editar</button>
+            <button className={styles.Button_edit}>Eliminar</button>
+          </div>
         </div>
-        <button className={styles.Button_edit}>Editar</button>
-      </div>
+      ))}
     </>
   );
 };
--- src/screens/Administrator_list_institutions.jsx
+++ src/screens/Administrator_list_institutions.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import Styles from '../styles/Administrator_institution.module.css'
-import NavBar from '../components/NavBar_Administrator'
+import React, { useState, useEffect } from 'react';
+import Styles from '../styles/Administrator_institution.module.css';
+import NavBar from '../components/NavBar_Administrator';
+import axios from 'axios';
 
 export const Administrator_list_institutions = () => {
+  const [institutions, setInstitutions] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/v1/getInstitutions');
+        setInstitutions(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
-        <NavBar/>
-        <h1 className={Styles.Titulo}>INSTITUCIONES</h1>
-
-        <div className={Styles.Institution_space}>
-            <div className={Styles.Institution_info}>
-                <p>Codigo de la institucion: ###</p>
-                <p>Nombre de la institucion: ###</p>
-            </div>
-            <button className={Styles.Button_edit}>Editar</button>
+      <NavBar />
+      <h1 className={Styles.Titulo}>INSTITUCIONES</h1>
+      {institutions.map((institution) => (
+        <div key={institution.Id_Institution} className={Styles.Institution_space}>
+          <div className={Styles.Institution_info}>
+            <p>Codigo de la institucion: {institution.Id_Institution}</p>
+            <p>Nombre de la institucion: {institution.Name_institution}</p>
+          </div>
+          <button className={Styles.Button_edit}>Editar</button>
         </div>
-
-        <div className={Styles.Institution_space}>
-            <div className={Styles.Institution_info}>
-                <p>Codigo de la institucion: ###</p>
-                <p>Nombre de la institucion: ###</p>
-            </div>
-            <button className={Styles.Button_edit}>Editar</button>
-        </div>
+      ))}
     </>
-  )
-}
+  );
+};
